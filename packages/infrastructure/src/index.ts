e7b385691e489866d4337dbfe2b2c751eeaec0d5
Riquelme Chile <sebastian.riquelme@hotmail.cl>
2026-07-26 15:23:34 -0400

    feat(runtime): export outbox repositories

--- packages/infrastructure/src/index.ts
+++ packages/infrastructure/src/index.ts
@@ -1,3 +1,5 @@
+export * from "./inMemoryOutboxRepository.js";
 export * from "./inMemoryRepositories.js";
 export * from "./postgresActionRepository.js";
+export * from "./postgresOutboxRepository.js";
 export * from "./postgresRepositories.js";
