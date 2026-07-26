3bbb3ce3e8187e4e75c92e6aa40861af7dd0c6e5
Riquelme Chile <sebastian.riquelme@hotmail.cl>
2026-07-26 15:49:28 -0400

    feat(security): export operator session repositories

--- packages/infrastructure/src/index.ts
+++ packages/infrastructure/src/index.ts
@@ -1,5 +1,7 @@
 export * from "./inMemoryOutboxRepository.js";
 export * from "./inMemoryRepositories.js";
+export * from "./inMemorySessionRepository.js";
 export * from "./postgresActionRepository.js";
 export * from "./postgresOutboxRepository.js";
 export * from "./postgresRepositories.js";
+export * from "./postgresSessionRepository.js";
