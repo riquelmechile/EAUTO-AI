9a6a8ff85d49541efa1859d3606dc58531896ce8
Riquelme Chile <sebastian.riquelme@hotmail.cl>
2026-07-26 23:33:13 -0400

    feat: export governed work order service

--- packages/application/src/index.ts
+++ packages/application/src/index.ts
@@ -2,6 +2,7 @@ export * from "./actionService.js";
 export * from "./agentOsService.js";
 export * from "./contentStudioService.js";
 export * from "./governedWorkOrderProcessor.js";
+export * from "./governedWorkOrderService.js";
 export * from "./llmService.js";
 export * from "./mercadoLibreService.js";
 export * from "./operationalIntelligenceService.js";
