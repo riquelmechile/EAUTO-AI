50caf3c691318069c4a6a088577b5e88397484f7
Riquelme Chile <sebastian.riquelme@hotmail.cl>
2026-07-28 19:25:40 -0400

    feat(content): export MiniMax creative provider

--- packages/content/src/index.ts
+++ packages/content/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./deterministicContentProvider.js";
 export * from "./deterministicProductVisionProvider.js";
 export * from "./httpContentProvider.js";
+export * from "./minimaxContentProvider.js";
